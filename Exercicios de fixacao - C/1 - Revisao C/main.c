c1a502de3e6858e10a57b5c42dd8c111e6ccc130
Gustavo Rocha <118305894+gnrochabr@users.noreply.github.com>
2024-08-09 21:16:29 -0300

    Update main.c

--- Exercicios de fixacao - C/1 - Revisao C/main.c
+++ Exercicios de fixacao - C/1 - Revisao C/main.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-//Criando a estrutura do tipo aluno
-typedef struct{
-int matricula;
-char nome[100];
-char endereco[200];
-char data_nasc[10];
-} Aluno;
-
-//prototipos
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#define TAM_PADRAO 10
 
 // Criando a estrutura do tipo Aluno
 typedef struct {
@@ -30,13 +18,15 @@ void ordenarTurma(Aluno *alunos[], int tamanho);
 void excluirAluno(Aluno *alunos[], int *tamanho);
 
 int main() {
-    int tamanho = 10;
+    int tamanho = TAM_PADRAO;
     Aluno *alunos[tamanho];
+    int matricula = 1;
     free(alunos);
     // Alocando memória para cada aluno e inicializando os valores
     for (int i = 0; i < tamanho; i++) {
         alunos[i] = (Aluno *)malloc(sizeof(Aluno));
-        alunos[i]->matricula = tamanho - i;
+        alunos[i]->matricula = matricula;
+        matricula++;
         sprintf(alunos[i]->nome, "Exemplo %d", i + 1);
         strcpy(alunos[i]->endereco, "Padrao");
         strcpy(alunos[i]->data_nasc, "Padrao");
@@ -45,13 +35,14 @@ int main() {
     // Chamando as funções
     int opcao = -1;
     while(opcao!=0){
-        printf("\nMenu do Sistema\n1 - Buscar por matricula\n2 - Ordenar por nome\n3 - Excluir aluno\n4 - Imprimir lista de alunos\n0 - Sair\nDigite a opcao desejada:");
+        printf("\nMenu do Sistema\n1 - Buscar por matricula\n2 - Ordenar por nome\n3 - Excluir aluno\n4 - Imprimir lista de alunos\n5  -Inserir novo aluno\n0 - Sair\nDigite a opcao desejada:");
         scanf("%d",&opcao);
         switch(opcao){
         case 1: pesquisaAluno(alunos,tamanho); break;
         case 2: ordenarTurma(alunos,tamanho); break;
         case 3: excluirAluno(alunos,&tamanho); break;
         case 4: imprimirVetor(alunos,tamanho); break;
+        case 5: insereAluno(alunos,&tamanho,&matricula); break;
         case 0:  // Liberando a memória alocada
         for (int i = 0; i < tamanho; i++) {
         free(alunos[i]);
@@ -100,7 +91,6 @@ void ordenarTurma(Aluno *alunos[], int tamanho) {
     }
     printf("\nVetor ordenado:\n");
     imprimirVetor(alunos, tamanho);
-    free(temp);
 }
 
 void excluirAluno(Aluno *alunos[], int *tamanho) {
@@ -114,59 +104,12 @@ void excluirAluno(Aluno *alunos[], int *tamanho) {
         }
     }
     if (ind >= 0) {
-        free(alunos[ind]);
         for (int j = ind; j < *tamanho - 1; j++) {
             alunos[j] = alunos[j + 1];
         }
-        free(alunos[*tamanho]);
         (*tamanho)--;
         printf("Aluno com matricula %d excluido.\n\n", matr);
     } else {
         printf("Aluno nao encontrado!\n\n");
     }
 }
-
-
-/* Base para desenvolvimento da tarefa
-    int main(){
-    int tamanho = 10;
-    Aluno *alunos[tamanho];
-    //Aluno novoAluno;
-
-    for(int i = 0; i<10; i++){
-        alunos[i] = (Aluno *)malloc(sizeof(Aluno));
-        alunos[i]->matricula = i+1;
-        sprintf(alunos[i]->nome,"Exemplo %d",i+1);
-        strcpy(alunos[i]->endereco,"Padrao");
-        strcpy(alunos[i]->data_nasc,"Padrao");
-    }
-
-    for(int j = 0; j<10; j++){
-        printf("%i - %s\n",alunos[j]->matricula,alunos[j]->nome);
-    }
-    pesquisaAluno(alunos,tamanho);
-    free(alunos);
-    return 0;
-}
-
-void pesquisaAluno(Aluno *alunos[],int tamanho){
- 
- int matr, ind = -1;
- printf("Digite a matrícula: ");
- scanf("%d",&matr);
-for(int j = 0; j<tamanho;j++){
-        if(alunos[j]->matricula == matr){
-            ind = j;
-            break;
-        }
-}
-if(ind >= 0)
-{
-    printf("O aluno %s, matricula %d foi encontrado!",alunos[ind]->nome,alunos[ind]->matricula);
-}
-else
-{
-    printf("Aluno não encontrado!");
-}
-
-}*/
